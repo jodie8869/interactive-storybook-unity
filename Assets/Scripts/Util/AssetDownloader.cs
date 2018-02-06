ca7df3cab178c2da46232eba7de7600d3616f6af
Hanna Lee <hannalee995@gmail.com>
2018-02-06 12:33:18 -0500

    Finish dynamic asset loading, can download audio clips in addition to images after adding audio files to Amazon S3

--- Assets/Scripts/Util/AssetDownloader.cs
+++ Assets/Scripts/Util/AssetDownloader.cs
@@ -22,12 +22,6 @@ public class AssetDownloader : MonoBehaviour {
 
     }
 
-    // Return true if the download has completed.
-    public bool checkDownloadComplete() {
-        // TODO: add audio clips count too
-        return (this.sprites.Count == this.expectedNumSprites) ;
-    }
-
     // Called to download the images and audio files needed for a particular story.
     // The arguments are lists of strings that should be the identifying part of the URL to
     // download from, and those strings will be used as keys into StoryManager's dictionaries
@@ -35,7 +29,7 @@ public class AssetDownloader : MonoBehaviour {
     public IEnumerator DownloadStoryAssets(string storyName, List<string> imageFileNames,
                                            List<string> audioFileNames,
                                            Action<Dictionary<string, Sprite>,
-                                           Dictionary<string, AudioClip>> callback) {
+                                           Dictionary<string, AudioClip>> onDownloadComplete) {
 
         // Duplication of clearing, but just to be safe.
         this.sprites.Clear();
@@ -45,17 +39,17 @@ public class AssetDownloader : MonoBehaviour {
         this.expectedNumAudioClips = audioFileNames.Count;
 
         foreach (string iFile in imageFileNames) {
-            StartCoroutine(downloadImage(storyName, iFile, callback));
+            StartCoroutine(downloadImage(storyName, iFile, onDownloadComplete));
         }
-        //foreach (string aFile in audioFileNames) {
-        //    StartCoroutine(downloadAudio(storyName, aFile));
-        //}
+        foreach (string aFile in audioFileNames) {
+            StartCoroutine(downloadAudio(storyName, aFile, onDownloadComplete));
+        }
         yield return null;
     }
 
     private IEnumerator downloadImage(string storyName, string imageFile,
                                       Action<Dictionary<string, Sprite>,
-                                      Dictionary<string, AudioClip>> callback) {
+                                      Dictionary<string, AudioClip>> onDownloadComplete) {
         string url = Constants.IMAGE_BASE_URL + storyName + "/" + imageFile + ".png?raw=1";
         // Using yield return for the new www object will wait until the download is complete
         // but without blocking the rest of the game.
@@ -66,27 +60,37 @@ public class AssetDownloader : MonoBehaviour {
                                       new Vector2(0, 0));
         this.sprites[imageFile] = sprite;
         Logger.Log("completed download of image " + imageFile);
-        if (this.checkDownloadComplete()) {
+        this.checkEntireDownloadComplete(onDownloadComplete);
+    }
+
+    private IEnumerator downloadAudio(string storyName, string audioFile,
+                                      Action<Dictionary<string, Sprite>,
+                                      Dictionary<string, AudioClip>> onDownloadComplete) {
+        string url = "https://s3.amazonaws.com/storycorpus-audio/" + storyName + "/" + audioFile + ".wav";
+        WWW www = new WWW(url);
+        yield return www;
+        AudioClip audioClip = www.GetAudioClip();
+        this.audioClips[audioFile] = audioClip;
+        Logger.Log("completed downloaded of audio " + audioFile);
+        this.checkEntireDownloadComplete(onDownloadComplete);
+    }
+
+    // If all files have been downloaded, call the onDownloadComplete callback with the newly
+    // downloaded assets, and then clear them out of our local concurrent dictionaries.
+    public void checkEntireDownloadComplete(Action<Dictionary<string, Sprite>,
+                                            Dictionary<string, AudioClip>> onDownloadComplete) {
+        if (this.sprites.Count == this.expectedNumSprites &&
+            this.audioClips.Count == this.expectedNumAudioClips)
+        {
             Dictionary<string, Sprite> nonConcurrentSprites =
                 new Dictionary<string, Sprite>(this.sprites);
             Dictionary<string, AudioClip> nonConcurrentAudioClips =
                 new Dictionary<string, AudioClip>(this.audioClips);
 
-            callback(nonConcurrentSprites, nonConcurrentAudioClips);
+            onDownloadComplete(nonConcurrentSprites, nonConcurrentAudioClips);
 
             this.sprites.Clear();
             this.audioClips.Clear();
         }
-    }
-
-    private IEnumerator downloadAudio(string storyName, string audioFile,
-                                      Action<Dictionary<string, Sprite>,
-                                      Dictionary<string, AudioClip>> callback)
-    {
-        string url = "https://www.dropbox.com/work/Story%20Corpus/audios/contentroot/stories/" + audioFile + ".wav";
-        WWW www = new WWW(url);
-        yield return www;
-        AudioClip audioClip = www.GetAudioClip();
-        this.audioClips[audioFile] = audioClip;
     }
 }
