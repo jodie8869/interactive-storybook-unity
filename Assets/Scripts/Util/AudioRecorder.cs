34394e964acb1ad84dbe07489fcaead5f55ed61b
Hanna Lee <hannalee995@gmail.com>
2018-02-12 20:36:04 -0500

    Empty skeleton of AudioRecorder

--- Assets/Scripts/Util/AudioRecorder.cs
+++ Assets/Scripts/Util/AudioRecorder.cs
@@ -1,17 +1,21 @@
+// A class for recording through the Android tablet microphone and saving the audio files.
+// This primarily used for reading assessment. Realtime interactions such as the child asking
+// for clarification on a word or something else like that will be done through Jibo ASR.
+
 using UnityEngine;
 using System.Collections;
 
-public class NewMonoBehaviour : MonoBehaviour
-{
-	// Use this for initialization
-	void Start()
-	{
+public class AudioRecorder : MonoBehaviour {
+	
+    AudioClip audioClip;
+
+    // Use this for initialization
+	void Start() {
 
 	}
 
 	// Update is called once per frame
-	void Update()
-	{
+	void Update() {
 			
 	}
 }
